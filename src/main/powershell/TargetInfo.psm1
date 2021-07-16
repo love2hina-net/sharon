bf682878c49dc161f82230f5442b190086be3270
love2hina <webmaster@love2hina.net>
2021-07-16 22:16:08 +0900

    フィールド列挙処理の前準備

--- src/main/powershell/TargetInfo.psm1
+++ src/main/powershell/TargetInfo.psm1
@@ -11,6 +11,8 @@ class ClassTargetInfo {
     # 完全名
     [string] $fullname
 
+    # フィールド
+    [FieldTargetInfo[]] $fields
 
     ClassTargetInfo([XPathNavigator]$node) {
 
@@ -19,6 +21,34 @@ class ClassTargetInfo {
         $this.name = $node.Evaluate('@name')
         $this.fullname = $node.Evaluate('@fullname')
 
+        $this.fields = @()
+        foreach ($i in $node.Evaluate('field')) {
+            $this.fields += [FieldTargetInfo]::new($i)
+        }
+    }
+
+}
+
+class FieldTargetInfo {
+
+    # コメント
+    [string] $comment
+    # 修飾子
+    [string] $modifier
+    # 型名
+    [string] $type
+    # 名前
+    [string] $name
+    # 初期値
+    [string] $value
+
+    FieldTargetInfo([XPathNavigator]$node) {
+
+        $this.comment = $node.Evaluate('comment/text()')
+        $this.modifier = $node.Evaluate('@modifier')
+        $this.type = $node.Evaluate('@type')
+        $this.name = $node.Evaluate('@name')
+        $this.value = $node.Evaluate('@value')
     }
 
 }
