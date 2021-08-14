6c9c5fa625e90ff4391f369f3cf56a0ff7b93378
love2hina <webmaster@love2hina.net>
2021-08-14 22:56:42 +0900

    SelectMany検証用

---
+++ src/test/powershell/regex.ps1
@@ -0,0 +1,18 @@
+﻿using namespace System.Collections.Generic
+using namespace System.Text.RegularExpressions
+using namespace System.Linq
+
+[Match] $match = [Regex]::Match('{#begin condition header:2 footer:1}', '\{#(\w+)(?:\s+(\S+))*\}')
+$captures = [Enumerable]::SelectMany($match.Groups, [Func[Group, IEnumerable[string]]] {
+    param([Group] $g)
+    return [Enumerable]::Select($g.Captures, [Func[Capture, string]] {
+        param([Capture] $c)
+        return [string] $c.Value
+    })
+})
+
+foreach ($capture in $captures) {
+    Write-Debug $capture
+}
+
+# https://github.com/PowerShell/PowerShell/issues/7651
