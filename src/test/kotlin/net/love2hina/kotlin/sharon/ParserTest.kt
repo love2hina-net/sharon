8028021d6ee8321703b396f636035a0b701a9534
love2hina <webmaster@love2hina.net>
2021-11-13 15:43:05 +0900

    ファイル列挙処理の実装中
    refs #10

--- src/test/kotlin/net/love2hina/kotlin/sharon/ParserTest.kt
+++ src/test/kotlin/net/love2hina/kotlin/sharon/ParserTest.kt
@@ -1,13 +1,12 @@
 package net.love2hina.kotlin.sharon
 
-import com.github.javaparser.utils.CodeGenerationUtils
+import net.love2hina.kotlin.sharon.TestUtils.pathProjectRoot
 import org.junit.jupiter.api.Test
 
 class ParserTest {
 
     @Test
     fun testParse() {
-        val pathProjectRoot = CodeGenerationUtils.mavenModuleRoot(ParserTest::class.java).resolve("../..")
         val pathSrcFile = pathProjectRoot.resolve("src/test/java/net/love2hina/kotlin/sharon/ParseTestTarget.java")
         val pathXmlFile = pathProjectRoot.resolve("test.xml")
 
